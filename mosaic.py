251804d57c76a006fc061135a0a248cd64ee0979
William Zay <Zay96@live.missouristate.edu>
2018-09-25 18:59:40 -0500

    Removed bead value print statement and added print statement showing number of beads detected.

--- mosaic.py
+++ mosaic.py
@@ -5,9 +5,13 @@ def main():
     stitch = stitching.Stitching()
     count = counting.Counting()
     circles = count.countBeads("./test/resources/sampleMaps/map.png")
+    validBeads = 0
     for i in circles[0,:]:
         beadInfo = count.getAvgColor(i,"./test/resources/sampleMaps/map.png")
-        print(beadInfo)
+        
+        if beadInfo[1] == False:
+            validBeads += 1
+    print("Valid beads detected: " + str(validBeads))
 
 
 if __name__ == "__main__":
