3383bfdcc826e17b5365fa22fac401278692676c
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-09-25 23:43:31 -0500

    add comment

--- lib/counting.py
+++ lib/counting.py
@@ -32,6 +32,7 @@ class Counting:
 
         circles = np.uint16(np.around(circles))
         for i in circles[0,:]:
+            # i[0] is x coordinate, i[1] is y coordinate, i[2] is radius
             # draw the outer circle
             cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
             # draw the center of the circle
