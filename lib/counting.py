baa472e983b7602f68b45f05d208167fa7c7ad7d
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-09-25 23:37:23 -0500

    refactor class to be more efficient

--- lib/counting.py
+++ lib/counting.py
@@ -9,23 +9,23 @@ import math
 """
 class Counting: 
 
-    def __init__(self):
-        pass
+    def __init__(self,imagePath):
+        self.imagePath = imagePath
+        self.grayScaleMap = cv2.imread(imagePath,0) # create grayscale cv2 img
+        self.colorMap = cv2.imread(imagePath) # create color cv2 img
+        self.colorBeads = []
+        self.waterBeads = []
 
     """
         Description: a function that takes a map of images and counts the beads.
-        @param imageMap - a map (image) of the microscope images.
         @return an object containing information collected during the counting process.
     """
-    def countBeads(self,imageMap):
+    def getColorBeads(self):
+        result = []
         # TODO: REMOVE ME BEFORE MERGING WITH MASTER
         # This is super helpful. apparently opencv is garbo at documenting this.
         # https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
-        img = cv2.imread(imageMap,0)
-<<<<<<< HEAD
-=======
-
->>>>>>> mlg
+        img = self.grayScaleMap
         cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
         circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,dp=1,minDist=40,
                             param1=50,param2=55,minRadius=0,maxRadius=75)
@@ -37,40 +37,26 @@ class Counting:
             # draw the center of the circle
             cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-        plt.imshow(cimg),plt.show()
-        return circles
+            color = self.getAvgColor(i)
+            if(color[1] == False): # if the bead is a water bead, leave it out.
+                self.colorBeads.append(color)
+                result.append(color)
+            else: 
+                self.waterBeads.append(color)
 
-    """
-        Description: a function that takes a cicle's RGB values and returns if it is water or not
-        @param RGB - tuple containing the average red, green, and blue values of a circle
-        @return a boolean that will be True if the circle is water
-    """ 
-    def isWater(self, RGB):
-        red = RGB[0]
-        green = RGB[1]
-        blue = RGB[2]
-        isWater = False
+        plt.imshow(cimg),plt.show() # show the beads that have been detected
+        return result 
 
-        # these may need to be adjusted, but a bead will either be white or blackish if small
-        maxRGBValue = 230
-        minRGBValue = 3
-
-        if red >= maxRGBValue and green >= maxRGBValue and blue >= maxRGBValue:
-            isWater = True
-        if red <= minRGBValue and green <= minRGBValue and blue <= minRGBValue:
-            isWater = True 
-        return isWater
 
     """
         Description: a function that takes an array representing a circle's[x-coord of center, y-coord of center, radius]
                     and returns an array containing the bead's average RGB values and a boolean isWater
         @param circleInfo - array that contains a circle's x and y coordinates of the center and the radius of the circle
-        @param imageMap - a map (image) of the microscope images in color.
         @return a list containing the RGB tuple and a boolean isWater
     """        
-    def getAvgColor(self, circleInfo, imageMap):
+    def getAvgColor(self, circleInfo):
         random.seed(0)
-        img = cv2.imread(imageMap)
+        img = self.colorMap
         imgY = img.shape[0]
         imgX = img.shape[1]
         r, g, b = [], [], []
@@ -82,8 +68,6 @@ class Counting:
         # buffer is to eliminate the outside shadows factoring in on the edges of beads
         buffer = math.ceil(0.3 * radius)
         
-<<<<<<< HEAD
-=======
         # 1st quadrant
         minX = x
         maxX = x + (radius - buffer)
@@ -156,8 +140,27 @@ class Counting:
         avgGreen = round(np.mean(g), 2)
         avgRed = round(np.mean(r), 2)
         averageRGB = (avgRed, avgGreen, avgBlue)
-        isWater = self.isWater(averageRGB)
+        isWater = self.__isWater(averageRGB)
 
         return [averageRGB, isWater]
         
->>>>>>> mlg
+    """
+        Description: a function that takes a cicle's RGB values and returns if it is water or not
+        @param RGB - tuple containing the average red, green, and blue values of a circle
+        @return a boolean that will be True if the circle is water
+    """ 
+    def __isWater(self, RGB):
+        red = RGB[0]
+        green = RGB[1]
+        blue = RGB[2]
+        isWater = False
+
+        # these may need to be adjusted, but a bead will either be white or blackish if small
+        maxRGBValue = 230
+        minRGBValue = 3
+
+        if red >= maxRGBValue and green >= maxRGBValue and blue >= maxRGBValue:
+            isWater = True
+        if red <= minRGBValue and green <= minRGBValue and blue <= minRGBValue:
+            isWater = True 
+        return isWater
--- mosaic.py
+++ mosaic.py
@@ -1,13 +1,15 @@
-import lib.stitching as stitching
-import lib.counting as counting
+from lib.stitching import Stitching 
+from lib.counting import Counting 
 
 def main():
-    stitch = stitching.Stitching()
-    count = counting.Counting()
-    circles = count.countBeads("./test/resources/sampleMaps/map.png")
-    for i in circles[0,:]:
-        beadInfo = count.getAvgColor(i,"./test/resources/sampleMaps/map.png")
-        print(beadInfo)
+    stitch = Stitching()
+    count = Counting("./test/resources/sampleMaps/map.png")
+    circles = count.getColorBeads()
+    print(circles)
+    print("Number of valid color beads found: "+str(len(circles)))
+    print("---------------printing water beads---------------")
+    print(count.waterBeads)
+    print("Number of water beads found: "+str(len(count.waterBeads)))
 
 
 if __name__ == "__main__":
