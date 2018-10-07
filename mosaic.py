a2ccf8a782ed64321e3df5f2fc777adbd621468a
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-10-07 02:32:44 -0500

    stash

--- mosaic.py
+++ mosaic.py
@@ -4,16 +4,16 @@ from matplotlib import pyplot as plt
 
 def main():
     stitcher = Stitching()
-    #stitcher.setDirectory("test/resources/sample3")
-    #imageMap = stitcher.stitchOrderedImages()
-    #plt.imshow(imageMap),plt.show() # show the beads that have been detected
+    stitcher.setDirectory("test/resources/sample3")
+    imageMap = stitcher.stitchOrderedImages()
+    plt.imshow(imageMap),plt.show() # show the beads that have been detected
 
     count = Counting("./test/resources/sampleMaps/map_10x.jpg")
     circles = count.getColorBeads(HoughConfig.OBJX10)
-    #for i in circles:
-       # print(i)
-  #  print("Number of valid color beads found: "+str(len(circles)))
-   # print("Number of water beads found: "+str(len(count.waterBeads)))
+    for i in circles:
+        print(i)
+    print("Number of valid color beads found: "+str(len(circles)))
+    print("Number of water beads found: "+str(len(count.waterBeads)))
 
 
 
