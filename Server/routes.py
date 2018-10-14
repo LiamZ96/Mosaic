5c60d30be65a63d11ccc4ca96ecc027337c55fc4
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-10-13 20:57:39 -0500

    add error redirect page

--- Server/routes.py
+++ Server/routes.py
@@ -9,6 +9,7 @@ ALLOWED_VIDEO_EXTENSIONS = set(['mp4'])
     Description: a function used to see if the uploaded file is in a valid format.
     @Param filename - name of the file being uploaded.
     @Param extensionList - set of allowed file extensions.
+    @return a boolean indicating whether the image is in an acceptable format or not.
 """
 def isFileAllowed(filename, extensionList):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in extensionList
@@ -23,10 +24,19 @@ def sendJs(path):
 def index():
     return render_template('index.html')
 
+@app.route('/error')
+def error():
+    errorMessage = request.args['errorMessage']
+    return render_template('error.html',error=errorMessage)
+
 @app.route('/uploadImages', methods=["POST"])
 def uploadImages(): 
     images = request.files.getlist("images")
     for i in images: 
+        #redirect to error page if the image is in an unacceptable
+        if(isFileAllowed(i.filename,ALLOWED_IMAGE_EXTENSIONS) == False): 
+            return redirect(url_for('error',errorMessage="One or more of the images that were uploaded are in the incorrect format. Accepted formats: "+(", ".join(ALLOWED_IMAGE_EXTENSIONS))))
+
         print("Image is permitted: "+str(isFileAllowed(i.filename,ALLOWED_IMAGE_EXTENSIONS))) #see if the image format is allowed
         print("Secure filename: "+str(secure_filename(i.filename))) #escape the filename
     
@@ -37,9 +47,13 @@ def uploadImages():
 @app.route('/uploadVideo', methods=["POST"])
 def uploadVideo(): 
     video = request.files['video']
+    #redirect to the error page if the video is not the correct format
+    if(isFileAllowed(video.filename,ALLOWED_VIDEO_EXTENSIONS) == False):
+        return redirect(url_for('error',errorMessage="The uploaded video is in the incorrect format. Accepted formats: "+(", ".join(ALLOWED_VIDEO_EXTENSIONS))))
 
     print("Video is permitted: "+str(isFileAllowed(video.filename,ALLOWED_VIDEO_EXTENSIONS))) #see if the image format is allowed
     print("Secure filename: "+str(secure_filename(video.filename))) #escape the filename
+
     #TODO: place video in a unique directory
     return redirect(url_for('index')) #redirect to homepage
 
