077ef96ba0cc591334ca3ab9165c558c01c23e90
Noah Z <zeilmannnoah@gmail.com>
2018-10-30 01:29:12 -0500

    Made view for choosing stitched image pretty

--- Server/resources/js/stitched.js
+++ Server/resources/js/stitched.js
@@ -1,4 +1,19 @@
 'use-strict'
 $(document).ready(function() {
-	
+	let buttonA = $('#buttonA'),
+		linkA = $('#linkA'),
+		buttonB = $('#buttonb'),
+		linkB = $('#linkA'),
+		overlay = $('#overlay');
+
+	$('.pannable-image').ImageViewer();
+	overlay.addClass('d-none');
+
+	buttonA.click(function(e) {
+		linkA.click();
+	});
+
+	buttonB.click(function(e) {
+
+	});
 });
